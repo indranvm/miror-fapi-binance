6119db767e3ad54ffcfd505cdae994ddd0d55f4b
indranvm <indranovemri@gmail.com>
2026-03-23 19:29:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 
-// CORS (optional)
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
 });
 
@@ -13,12 +15,17 @@ app.use("/", createProxyMiddleware({
   target: "https://fapi.binance.com",
   changeOrigin: true,
   secure: true,
+  headers: {
+    "X-Forwarded-For": "103.28.248.1", // IP Asia/ID
+    "CF-Connecting-IP": "103.28.248.1",
+    "X-Real-IP": "103.28.248.1",
+  },
   onProxyReq: (proxyReq) => {
     proxyReq.setHeader("Origin", "https://fapi.binance.com");
+    proxyReq.setHeader("Referer", "https://fapi.binance.com");
+    proxyReq.setHeader("X-Forwarded-For", "103.28.248.1");
   }
 }));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Proxy running on port " + PORT);
-});
+app.listen(PORT, () => console.log("Proxy running on port " + PORT));
